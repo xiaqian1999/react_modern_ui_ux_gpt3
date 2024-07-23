38189e2f0743a5c8727aefb3aee8d2cd2baa40b8
RoniaZhang <XiaQian1999@gmail.com>
2024-07-23 13:30:23 -0400

    React Project 1 - 2 more field done

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -3,7 +3,16 @@ import './cta.css';
 
 const Cta = () => {
   return (
-    <div>Cta</div>
+    <div className='gpt3__cta'>
+      <div className='gpt3__cta-content'>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+
+      <div className='gpt3__cta-btn'>
+        <button type="button">Get Started</button>
+      </div>
+    </div>
   )
 }
 
